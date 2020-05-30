09d04f851bb9e77c4611daa3041ce47e40bc9daf
elias.stalpaert <elias.stalpaert@student.kuleuven.be>
2020-05-30 21:02:10 +0200

    Removed JBoss Weld library

--- src/ejb/DemoBean.java
+++ src/ejb/DemoBean.java
@@ -1,18 +1,14 @@
 package ejb;
 
-import com.google.common.collect.ObjectArrays;
 import jpa.embeddables.Address;
 import jpa.embeddables.BarInfo;
 import jpa.embeddables.TimePeriod;
 import jpa.entities.*;
 import utilities.DaysOfTheWeek;
 
-import javax.ejb.Stateful;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.awt.*;
-import java.sql.SQLIntegrityConstraintViolationException;
 
 @Stateless(name = "DemoEJB")
 public class DemoBean {
@@ -24,7 +20,7 @@ public class DemoBean {
 
     public String epischeActie()
     {
-/*
+
         //Users, drankjes en de bar moeten apart gepersist worden
         Customer customer = new Customer("elias", "elias");
         entityManager.persist(customer);
@@ -32,18 +28,10 @@ public class DemoBean {
         entityManager.persist(baas);
         BarEmployee employee = new BarEmployee("emplo", "yee");
         entityManager.persist(employee);
-*/
-        Item drankje = new DrinkItem("bier" ,5.2f,33f);
-        try
-        {
-            entityManager.persist(drankje);
-        }
-        catch (Exception e)
-        {
-            return e.getMessage();
-        }
 
-        /*
+        Item drankje = new DrinkItem("bier" ,5.2f,33f);
+        entityManager.persist(drankje);
+
         Address address = new Address("tiense","leuven");
         Bar hdr = new Bar(new BarInfo("hdr",address),200);
         hdr.addToMenu(drankje, 1, 100);
@@ -51,9 +39,9 @@ public class DemoBean {
         hdr.addEmployeeToShift(employee, new TimePeriod(5,2), DaysOfTheWeek.FRIDAY);
         hdr.addReservation(customer, drankje, 2);
         entityManager.persist(hdr);
-*/
 
-        //return baas.getOwnedBars().get(0).getBarInfo().getName();
+
+        return baas.getOwnedBars().get(0).getBarInfo().getName();
 /*
         Address address = new Address("tiense","leuven");
         BarEmployee employee = entityManager.find(BarEmployee.class, "emplo");
@@ -67,6 +55,5 @@ public class DemoBean {
         return s;
 
          */
-    return "kaasbeer";
     }
 }
